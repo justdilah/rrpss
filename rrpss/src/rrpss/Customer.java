90fdbd8f8bd4d70e642c86baa80f12fdc34755d8
justdilah <nurdilah352@gmail.com>
2021-10-21 20:58:06 +0800

    test2

--- rrpss/src/rrpss/Customer.java
+++ rrpss/src/rrpss/Customer.java
@@ -2,8 +2,8 @@ package rrpss;
 
 public class Customer {
 	private Boolean membership;
-	int i;
-	int ZHENYI_IS_HANDSOME;
+	private int i;
+	private int ZHENYI_IS_HANDSOME;
 	private int sadlife2;
 	private boolean lasttry2;
 	private int yolo;
